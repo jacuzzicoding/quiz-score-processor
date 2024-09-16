b73c87fbdae33ecc9d4161bd8435b358bcb2437e
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-16 17:09:12 -0400

    Refactor quiz-processor.cpp and add method for calculating average into main()

--- source files/quiz-processor.cpp
+++ source files/quiz-processor.cpp
@@ -72,5 +72,18 @@ void sortArray() { // Define an array of integers
 int main() {
     readTextFile();
     stringToArray();
+
+// Define a 2D array of integers we will use to calculate the average of each row
+    const int rows = 3;
+    const int cols = 4;
+    int array[rows][cols] = {
+    {10, 20, 30, 40},
+    {50, 60, 70, 80},
+    {90, 100, 110, 120}
+    };
+    for (int i = 0; i < rows; i++) {
+    double avg = calculateAverage(array[i], cols);
+    std::cout << "Average of row " << i + 1 << " is: " << avg << std::endl;
+    }
     return 0;
 }
